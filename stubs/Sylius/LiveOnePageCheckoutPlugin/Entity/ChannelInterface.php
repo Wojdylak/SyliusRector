1e0802a8dfc5ed8cf392d16fed3e1673073d9e39
Wojdylak <karolwojdyla37@gmail.com>
2026-07-06 13:00:47 +0200

    Add rule for LiveOnePageCheckoutPlugin 01

---
+++ stubs/Sylius/LiveOnePageCheckoutPlugin/Entity/ChannelInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\LiveOnePageCheckoutPlugin\Entity;
+
+if (class_exists('Sylius\LiveOnePageCheckoutPlugin\Entity\ChannelInterface')) {
+    return;
+}
+
+interface ChannelInterface
+{
+}
---
+++ tests/Set/LiveOnePageCheckoutPlugin/LiveOnePageCheckoutPlugin01Test.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\SyliusRector\Tests\Set\LiveOnePageCheckoutPlugin;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class LiveOnePageCheckoutPlugin01Test extends AbstractRectorTestCase
+{
+    /** @dataProvider provideData() */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    /** @return Iterator<string> */
+    public function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/live_one_page_checkout_plugin_0_1.php';
+    }
+}
